157d55a20d64fc8b4d36a6246d830a4aea792d67
Abderrahmane Abdelouafi <ababdelo@e1r10p4.1337.ma>
2023-08-02 09:44:28 +0100

    Solving 'fizzbuzz'

---
+++ LEV1/Work/fizzbuzz.c
@@ -0,0 +1,36 @@
+#include <unistd.h>
+
+int	main()
+{
+	int index = 1;
+	int fst;
+	int snd;
+	
+	while (index <= 100)
+	{
+		if (index % 3 == 0 && index % 5 == 0)
+			write(1, "fizzbuzz", 8);
+		else if (index % 3 == 0)
+			write(1, "fizz", 4);
+		else if (index % 5 == 0)
+			write(1, "buzz", 4);
+		else
+		{	
+			if (index < 10)
+			{	
+				fst = index + '0';
+				write(1, &fst, 1);
+			}
+			else
+			{
+				fst = (index / 10) + '0';
+				snd = (index % 10) + '0';	
+				write(1, &fst, 1);
+				write(1, &snd, 1);
+			}
+		}
+		write(1, "\n", 1);
+		index++;	
+	}
+	return 0;
+}
