b0642fb436148eaad00e825bff6bc5bded9a22e8
ababdelo <ababdelo.imdstl@gmail.com>
2023-08-02 14:24:25 +0100

    updating 'search_and_replace'

--- LEV1/Work/search_and_replace.c
+++ LEV1/Work/search_and_replace.c
@@ -6,26 +6,15 @@
 /*   By: ababdelo <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/02 13:30:34 by ababdelo          #+#    #+#             */
-/*   Updated: 2023/08/02 14:20:28 by ababdelo         ###   ########.fr       */
+/*   Updated: 2023/08/02 14:23:56 by ababdelo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <unistd.h>
-#include <stdio.h>
-
-// int ft_strlen(char *str)
-// {
-// 	int index = -1;
-// 	while (str[++index])
-// 		;
-// 	return index;
-// }
 
 int main(int ac, char **av)
 {
 	int i = 0;
-	// int lenav2 = ft_strlen(av[2]);
-	// int lenav3 = ft_strlen(av[3]);
 	if (ac == 4)
 	{
 		while(av[1][i])
